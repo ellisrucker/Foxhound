ec37afdf257350f81bbb8045d56e7809062e39cc
Ellis Rucker <m.ellisrucker@gmail.com>
2019-08-01 11:49:05 -0400

    Add testing module

---
+++ Foxhound/src/test/java/CaseTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class CaseTest {
+
+    //Test Naming Conventions:
+    //MethodName_ExpectedBehavior_StateUnderTest
+
+    @Test
+    public void idToNumber_RemovesNonDigits_IfIdIsValid() {
+
+    }
+}
