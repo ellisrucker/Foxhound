075dd763cf7b2bca4661f3106525fdc7c92e1ea0
Ellis Rucker <m.ellisrucker@gmail.com>
2019-08-02 17:41:23 -0400

    Pass first successful unit tests

--- Foxhound/src/test/java/CaseTest.java
+++ Foxhound/src/test/java/CaseTest.java
@@ -1,5 +1,5 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class CaseTest {
 
@@ -7,10 +7,19 @@ public class CaseTest {
     //MethodName_ExpectedBehavior_StateUnderTest
 
     @Test
-    public void idToNumber_RemovesNonDigits_IfIdIsValid() {
+    public void idToNumber_returnsInteger_ifPassedString() {
         String id = "MONH1903022";
-        id = id.replaceAll( "\\D", "");
-        Integer.parseInt(id);
-        assertEquals(1903022,id);
+        id = id.replaceAll("\\D", "");
+        Integer idNumber = Integer.parseInt(id);
+        assertTrue(idNumber instanceof Integer);
+    }
+
+    @Test
+    public void idToNumber_removesLetters_ifPassedString() {
+        String id = "MONH1903022";
+        Integer expectedResult = 1903022;
+        id = id.replaceAll("\\D", "");
+        Integer idNumber = Integer.parseInt(id);
+        assertEquals(expectedResult, idNumber);
     }
 }
