234e85e83f500f42e74fefd263178767b87f599f
Ellis Rucker <m.ellisrucker@gmail.com>
2019-09-11 17:37:04 -0400

    Add ParsedRow class

--- Foxhound/src/main/java/SeparatedRow.java
+++ Foxhound/src/main/java/SeparatedRow.java
@@ -2,21 +2,22 @@
 public class SeparatedRow {
 
     //Object representation of current row
-    String date;
-    String motherName;
-    String maternalPatientId;
-    String paternalPatientId;
-    String gestationGender;
-    String testTypeCost;
-    String referral;
-    String genotypeA;
-    String genotypeB;
-    String firstDraw;
-    String secondDraw;
-    String thirdDraw;
-    String result;
-    String confirmation;
-    
+    //Each cell of currentRow becomes a String attribute
+    private String date;
+    private String motherName;
+    private String maternalPatientId;
+    private String paternalPatientId;
+    private String gestationGender;
+    private String testTypeCost;
+    private String referral;
+    private String genotypeA;
+    private String genotypeB;
+    private String firstDraw;
+    private String secondDraw;
+    private String thirdDraw;
+    private String result;
+    private String confirmation;
+
 
 
 }
